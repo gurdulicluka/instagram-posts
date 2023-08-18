f76b721e4e2cd71c1af5a020d30a8228ebed7324
Luka Gurdulic <phallondev@gmail.com>
2023-08-18 13:12:47 +0200

    Add on load animation and finish component

--- custom_credit_card/src/app.js
+++ custom_credit_card/src/app.js
@@ -1,22 +1,35 @@
 const gradientLabels = document.querySelectorAll(".custom-radio");
+const gradients = document.querySelectorAll(".color");
+let currentGradient = document.querySelector(".fullPreview");
 
-gradientLabels.forEach((element) => {
-  const checkbox = element.previousElementSibling;
-  const gradientBackground = document.querySelector(`.${checkbox.id}`);
+gradientLabels.forEach((label) => {
+  const checkbox = label.previousElementSibling; //checkbox input
+  const gradientBackground = document.querySelector(`.${checkbox.id}`); //gradient bg
+
+  label.addEventListener("mouseover", function () {
+    gradientBackground.style.zIndex = 0;
+    gradientBackground.classList.add("previewOn");
+    gradientBackground.classList.remove("previewOut");
+  });
+
+  label.addEventListener("mouseleave", function () {
+    gradientBackground.classList.remove("previewOn");
+    gradientBackground.classList.add("previewOut");
+  });
 
   checkbox.addEventListener("change", function () {
-    if (checkbox.checked) {
-      element.style.pointerEvents = "none";
-    } else {
-      element.style.pointerEvents = "auto";
-    }
-  });
+    gradients.forEach((el) => {
+      el.classList.remove("previewOut");
 
-  element.addEventListener("mouseover", function () {
-    gradientBackground.style.display = "block";
-  });
+      if (el !== gradientBackground) {
+        el.style.zIndex = -2;
+        setTimeout(() => {
+          el.classList.remove("fullPreview");
+        }, 400);
+      }
+    });
 
-  element.addEventListener("mouseleave", function () {
-    gradientBackground.style.display = "none";
+    gradientBackground.style.zIndex = -1;
+    gradientBackground.classList.add("fullPreview");
   });
 });
